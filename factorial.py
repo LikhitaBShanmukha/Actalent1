3ad1520e9b1f1347168b4d5e355e4b8157f562ef
LikhitaBShanmukha <113893538+LikhitaBShanmukha@users.noreply.github.com>
2022-09-19 17:04:20 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,7 +1,19 @@
-f=1
-for i in range(1,50)
-	for k in range(2,i)
-		f=f*k
+def factorial(n):
+	if n < 0:
+		return 0
+	elif n == 0 or n == 1:
+		return 1
+	else:
+		fact = 1
+		while(n > 1):
+			fact *= n
+			n -= 1
+		return fact
 
-print(f)
+
+num = 5;
+print("Factorial of",num,"is",
+factorial(num))
+
+
 		
